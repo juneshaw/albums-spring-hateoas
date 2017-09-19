827d8ed05a3979ad5db182e46ee00b5c3ba9f28f
June Shaw <june.shaw@me.com>
2017-09-18 21:19:23 -0600

    purchase album

--- src/main/java/com/cor/controller/AlbumController.java
+++ src/main/java/com/cor/controller/AlbumController.java
@@ -18,21 +18,21 @@ import com.cor.service.MusicService;
 
 @Controller
 public class AlbumController {
-	
+
 	@Autowired
 	private MusicService musicService;
 
 	@RequestMapping(value = "/albums", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public Collection<Resource<Album>> getAllAlbums() {
-		
+
 		Collection<Album> albums = musicService.getAllAlbums();
 		List<Resource<Album>> resources = new ArrayList<Resource<Album>>();
 		for (Album album : albums) {
 			resources.add(getAlbumResource(album));
 		}
 		return resources;
-		
+
 	}
 
 	@RequestMapping(value = "/album/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -64,12 +64,20 @@ public class AlbumController {
 	@RequestMapping(value = "/album/purchase/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public Resource<Album> purchaseAlbum(@PathVariable(value = "id") String id) {
-		
-		Album a = musicService.getAlbum(id);
-		a.setStockLevel(a.getStockLevel() - 1);
-		Resource<Album> resource = new Resource<Album>(a);
+
+		Album album = musicService.getAlbum(id);
+		album.setStockLevel(album.getStockLevel() - 1);
+		Resource<Album> resource = new Resource<Album>(album);
+
+		// Link to Album
 		resource.add(linkTo(methodOn(AlbumController.class).getAlbum(id)).withSelfRel());
+		// Link to Artist
+		resource.add(linkTo(methodOn(ArtistController.class).getArtist(album.getArtist().getId())).withRel("artist"));
+		// Option to purchase Album
+		if (album.getStockLevel() > 0) {
+			resource.add(linkTo(methodOn(AlbumController.class).purchaseAlbum(album.getId())).withRel("album.purchase"));
+		}
 		return resource;
-		
+
 	}
 }
